832b092318f73f618170fffb779e43e24159ca39
texhno <texhno@dmz.rs>
2024-06-07 15:06:30 +0200

    Added single cdp page functionality and styles

---
+++ src/hooks/useFinance.ts
@@ -0,0 +1,115 @@
+import { utils } from "@defisaver/tokens";
+import { useContext, useState } from "react";
+
+import GlobalContext from "@/context/GlobalContext";
+import { CollateralType } from "@/types/globalTypes";
+import { useCdpContract } from "@/hooks/useCdpContract";
+
+export const useFinance = (cdpId: string) => {
+  const { cdpContract, ilksContract, provider } = useContext(GlobalContext);
+  const {
+    totalDebt,
+    fetchCdpById,
+    getCollateralRate,
+    getCollateralPrice,
+    getLiquidationRationForCollateral,
+  } = useCdpContract({
+    provider: provider!,
+    cdpContract: cdpContract!,
+    ilksContract: ilksContract!,
+  });
+  const [debt, setDebt] = useState<number>();
+  const [owner, setOwner] = useState<string>();
+  const [collateral, setCollateral] = useState<number>();
+  const [liquidationFee, setLiquidationFee] = useState<number>();
+  const [collateralPrice, setCollateralPrice] = useState<number>();
+  const [liquidationRatio, setLiquidationRatio] = useState<number>();
+  const [collateralType, setCollateralType] = useState<CollateralType>();
+  const [collateralUsdValue, setCollateralUsdValue] = useState<number>();
+  const [collateralizationRatio, setCollateralizationRatio] =
+    useState<number>();
+  const [
+    maxDebtRemainingBeforeLiquidation,
+    setMaxDebtRemainingBeforeLiquidation,
+  ] = useState<number>();
+  const [
+    maxCollateralWithdrawBeforeLiquidation,
+    setMaxCollateralWithdrawBeforeLiquidation,
+  ] = useState<number>();
+
+  const getFinancialData = async () => {
+    const cdp = await fetchCdpById(+cdpId!);
+    setOwner(cdp.owner);
+    const collateralType = utils.bytesToString(cdp.ilk);
+    const colRate = await getCollateralRate(
+      CollateralType[collateralType as keyof typeof CollateralType]
+    );
+    const debt = cdp.debt;
+    const fullDebt = totalDebt(debt, colRate) / 1e9;
+    const [colPrice, liqRatio] = await Promise.all([
+      getCollateralPrice(
+        CollateralType[collateralType as keyof typeof CollateralType]
+      ),
+      getLiquidationRationForCollateral(
+        CollateralType[collateralType as keyof typeof CollateralType]
+      ),
+    ]);
+
+    const collateral = +parseFloat(
+      provider!.utils.fromWei(cdp.collateral, "ether")
+    );
+
+    setCollateral(collateral);
+
+    setCollateralType(
+      CollateralType[collateralType as keyof typeof CollateralType]
+    );
+
+    setDebt(fullDebt);
+
+    const collateralUsdValue = collateral * colPrice;
+
+    setCollateralUsdValue(collateralUsdValue);
+
+    const LIQUIDATION_FEE_PERCENTAGE = 13;
+    const liquidationPrice =
+      (+provider!.utils.fromWei(debt, "ether") * LIQUIDATION_FEE_PERCENTAGE) /
+      100;
+    const collRatio = (collateralUsdValue / fullDebt) * 100;
+    const minCollateralValueBeforeLiquidation = fullDebt * 1.5;
+    const minCollateralBeforeLiquidation =
+      minCollateralValueBeforeLiquidation / colPrice;
+    const maxDebtBeforeLiquidation = collateralUsdValue * 1.5;
+    const maxCollWithdrawBeforeLiquidation = (
+      collateral - minCollateralBeforeLiquidation
+    ).toFixed(2);
+    const maxDebtIncurBeforeLiquidation = (
+      maxDebtBeforeLiquidation - fullDebt
+    ).toFixed(2);
+
+    setLiquidationFee(liquidationPrice);
+    setMaxCollateralWithdrawBeforeLiquidation(
+      +maxCollWithdrawBeforeLiquidation
+    );
+    setMaxDebtRemainingBeforeLiquidation(+maxDebtIncurBeforeLiquidation);
+    setCollateralizationRatio(collRatio);
+
+    setCollateralPrice(colPrice);
+    setLiquidationRatio(liqRatio);
+  };
+
+  return {
+    debt,
+    owner,
+    collateral,
+    liquidationFee,
+    collateralType,
+    collateralPrice,
+    getFinancialData,
+    liquidationRatio,
+    collateralUsdValue,
+    collateralizationRatio,
+    maxDebtRemainingBeforeLiquidation,
+    maxCollateralWithdrawBeforeLiquidation,
+  };
+};
--- src/views/cdp/ViewCdp.tsx
+++ src/views/cdp/ViewCdp.tsx
@@ -1,119 +1,133 @@
-/* eslint-disable no-extra-boolean-cast */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { utils } from "@defisaver/tokens";
+import { FC, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { FC, useContext, useEffect, useState } from "react";
 
-import GlobalContext from "@/context/GlobalContext";
-import { CollateralType } from "@/types/globalTypes";
-import { useCdpContract } from "@/hooks/useCdpContract";
+import "./ViewCdp.scss";
+import { useFinance } from "@/hooks/useFinance";
+import { formatAccount } from "@/utils/strings";
 
 const ViewCdp: FC = () => {
   const { cdpId } = useParams();
-  const { cdpContract, ilksContract, provider } = useContext(GlobalContext);
   const {
-    totalDebt,
-    fetchCdpById,
-    getCollateralRate,
-    getCollateralPrice,
-    getLiquidationRationForCollateral,
-  } = useCdpContract({
-    provider: provider!,
-    cdpContract: cdpContract!,
-    ilksContract: ilksContract!,
-  });
-  const [debt, setDebt] = useState<number>();
-  const [collateral, setCollateral] = useState<number>();
-  const [collateralPrice, setCollateralPrice] = useState<number>();
-  const [liquidationRatio, setLiquidationRatio] = useState<number>();
-  const [collateralType, setCollateralType] = useState<CollateralType>();
-  const [collateralUsdValue, setCollateralUsdValue] = useState<number>();
-  // const [canWithdrawWithoutLiquidation, setCanWithdrawWithoutLiquidation] =
-  //   useState<number>();
+    debt,
+    owner,
+    collateral,
+    liquidationFee,
+    collateralType,
+    collateralPrice,
+    // liquidationRatio,
+    getFinancialData,
+    // collateralUsdValue,
+    collateralizationRatio,
+    maxDebtRemainingBeforeLiquidation,
+    maxCollateralWithdrawBeforeLiquidation,
+  } = useFinance(cdpId!);
 
   useEffect(() => {
-    (async () => {
-      const cdp = await fetchCdpById(+cdpId!);
-
-      const collateral = +parseFloat(
-        provider!.utils.fromWei(cdp.collateral, "ether")
-      );
-
-      setCollateral(collateral);
-
-      const collateralType = utils.bytesToString(cdp.ilk);
-      setCollateralType(
-        CollateralType[collateralType as keyof typeof CollateralType]
-      );
-
-      const colRate = await getCollateralRate(
-        CollateralType[collateralType as keyof typeof CollateralType]
-      );
-
-      const debt = cdp.debt;
-      const fullDebt = totalDebt(debt, colRate);
-
-      setDebt(fullDebt);
-
-      const [colPrice, liqRatio] = await Promise.all([
-        getCollateralPrice(
-          CollateralType[collateralType as keyof typeof CollateralType]
-        ),
-        getLiquidationRationForCollateral(
-          CollateralType[collateralType as keyof typeof CollateralType]
-        ),
-      ]);
-
-      const collateralUsdValue = collateral * colPrice;
-
-      setCollateralUsdValue(collateralUsdValue);
-
-      setCollateralPrice(colPrice);
-      setLiquidationRatio(liqRatio);
-    })();
+    getFinancialData();
   }, []);
 
   return (
-    <div className="single-cdp">
-      <div className="col-price">
-        Collateral price:
-        {collateralPrice !== undefined ? (
-          <p>{collateralPrice}</p>
+    <div className="single-cdp-card">
+      <div className="cdp-header">
+        <span>Owner: </span>
+        {owner !== undefined ? (
+          <p>{formatAccount(owner)}</p>
         ) : (
           <p>Loading...</p>
         )}
       </div>
-      <div className="cdp-liqidation-ratio">
-        Liquidation ratio:
-        {liquidationRatio !== undefined ? (
-          <p>{liquidationRatio}</p>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-      <div className="col-value">
-        Collateral:
-        {collateral !== undefined ? (
-          <p>
-            {collateral!.toFixed(2)}
-            {collateralType}
-            {}
-          </p>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-      <div className="total-collateral-value-usd">
-        Total collateral USD value:
-        {collateralUsdValue !== undefined ? (
-          <p>${collateralUsdValue.toFixed(2)}</p>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-      <div className="cdp-debt">
-        Debt:{" "}
-        {debt !== undefined ? <p>{debt.toFixed(2)}</p> : <p>Loading...</p>}
+      <div className="cdp-rows">
+        <div className="cdp-row">
+          <div className="cdp-detail">
+            <p className="faded-text">Collateral:</p>
+            {collateral !== undefined ? (
+              <p>
+                {collateral!.toFixed(2)}
+                <span className="faded-text">{collateralType}</span>
+              </p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          {/* <div className="cdp-detail">
+            Total collateral USD value:
+            {collateralUsdValue !== undefined ? (
+              <p>${collateralUsdValue.toFixed(2)}</p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div> */}
+          <div className="cdp-detail">
+            <p className="faded-text">Debt:</p>
+            {debt !== undefined ? (
+              <p>
+                {debt.toFixed(2)}
+                <span className="faded-text">DAI</span>
+              </p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          <div className="cdp-detail">
+            <p className="faded-text">Ratio:</p>
+            {collateralizationRatio !== undefined ? (
+              <p>{collateralizationRatio.toFixed(2)}%</p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          {/* <div className="cdp-detail">
+            Liquidation ratio:
+            {liquidationRatio !== undefined ? (
+              <p>{liquidationRatio.toFixed(2)}</p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div> */}
+        </div>
+        <div className="cdp-row">
+          <div className="cdp-detail">
+            <p className="faded-text">Safe to withdraw:</p>
+            {maxCollateralWithdrawBeforeLiquidation !== undefined ? (
+              <p>
+                {maxCollateralWithdrawBeforeLiquidation}
+                <span className="faded-text">{collateralType}</span>
+              </p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          <div className="cdp-detail">
+            <p className="faded-text">Safe to indebt:</p>
+            {maxDebtRemainingBeforeLiquidation !== undefined ? (
+              <p>
+                {maxDebtRemainingBeforeLiquidation?.toFixed(2)}
+                <span className="faded-text">DAI</span>
+              </p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        </div>
+        <div className="cdp-row">
+          <div className="cdp-detail">
+            <p className="faded-text">Collateral price:</p>
+            {collateralPrice !== undefined ? (
+              <p>${collateralPrice.toFixed(2)}</p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          <div className="cdp-detail">
+            <p className="faded-text">Liquidation fee:</p>
+            {liquidationFee != undefined ? (
+              <p>${liquidationFee.toFixed(2)}</p>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
